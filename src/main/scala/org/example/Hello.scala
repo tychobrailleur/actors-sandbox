43960c397935656bf892a9087797e41409738fbc
Sébastien Le Callonnec <sebastien@weblogism.com>
2015-06-01 22:11:37 +0100

    Add scheduled call.

--- src/main/scala/org/example/Hello.scala
+++ src/main/scala/org/example/Hello.scala
@@ -1,12 +1,32 @@
 package org.example
 
 import akka.actor._
+import scala.language.postfixOps
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
+
+
+class MyOtherActor extends Actor {
+  def receive = {
+    case _ =>  {
+      println("Hello again!")
+      context.system.shutdown()
+    }
+  }
+}
+
+object MyOtherActor {
+  trait Operation
+  case object Hello extends Operation
+}
 
 class MyActor extends Actor {
   def receive = {
     case _ => {
       println("Hello!")
-      context.system.shutdown()
+
+      val myOtherActor = context.actorOf(Props[MyOtherActor], "otherCreator")
+      context.system.scheduler.schedule(1 second, 1 second, myOtherActor, Hello)
     }
   }
 }
